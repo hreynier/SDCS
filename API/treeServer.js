935bf4814a2b6e59ed1525c7302c3c67b8011289
Harvey Reynier <harveyreynier@gmail.com>
2020-05-09 21:06:53 +0100

    Added endpoint for heatmap.

--- API/treeServer.js
+++ API/treeServer.js
@@ -190,6 +190,45 @@ var server = app.listen(portNumber, function () {
     console.log('App listening at http://%s:%s', host, port);
 })
 
+//  API EndPoint to get spatial & sustainability (carbon, pollution etc) data for all trees in camden.
+app.get('/data/sust/:weight', function (req, res) {
+
+    // Allows data to be downloaded from the server with security concerns
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-WithD");
+    // If all the variables are provided connect to the database
+    if(req.params.weight != ""){
+             
+              // Parse the values from the URL into numbers for the query
+              //var Lat = parseFloat(req.params.lat);
+              //var Lon = parseFloat(req.params.lon);
+              var weight = mysql_real_escape_string(req.params.weight);
+
+
+              // SQL Statement to run
+              var sql = "SELECT l.id, l.Lat, l.Lon, s."+weight+" FROM tree_locations l INNER JOIN sust_data s ON l.id = s.id;"
+              
+              // Log it on the screen for debugging
+              console.log(sql);
+
+              // Run the SQL Query
+              connection.query(sql, function(err, rows, fields) {
+                      if (err) console.log("Err:" + err);
+                      if(rows != undefined){
+                              // If we have data that comes bag send it to the user.
+                              res.send(rows);
+                      }else{
+                              res.send("");
+                      }
+              });
+      }else{
+              // If all the URL variables are not passed send an empty string to the user
+              res.send("");
+      }
+});
+
+
+
 
 //  Function to prevent SQL injection on string API requests.
 function mysql_real_escape_string (str) {
