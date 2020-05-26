6837018211f7e2e9a263fadc6c519c2f8060f3b9
Harvey Reynier <harveyreynier@gmail.com>
2020-05-26 16:45:40 +0100

    formatting.

--- publicHTML/JS/toolkit.js
+++ publicHTML/JS/toolkit.js
@@ -189,7 +189,7 @@ $('body').scrollspy({
 });
 
 /* smooth scrolling sections - Adapted from Bootstrap 4 guide */
-$('a[href*=#]:not([href=#])').click(function() {
+$('a[href*=\\#]:not([href=\\#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
